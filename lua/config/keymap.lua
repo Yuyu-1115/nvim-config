8a34701c141231cae72a9b8af0cf27f6362682e0
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-11-09 15:00:32 +0800

    add some keymapping

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -5,3 +5,9 @@ vim.api.nvim_set_keymap("n", "<Leader>w", "<C-w>", { noremap = true, silent = tr
 vim.api.nvim_set_keymap('n', 'H', '^', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'L', '$', { noremap = true, silent = true })
 
+-- clear search highlighting
+vim.api.nvim_set_keymap("n", "<Leader>cl", ":nohlsearch<CR>", { noremap = true, silent = true })
+
+-- LSP keymaps
+-- rename
+vim.api.nvim_set_keymap("n", "<Leader>rn", "<CMD>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
