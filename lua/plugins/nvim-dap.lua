87c765db2a4b5792d202fb1d9bcccd4345943afc
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-12-04 13:33:13 +0800

    nvim-dap: init

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,187 @@
+return {
+	"mfussenegger/nvim-dap",
+	event = "VeryLazy",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"jay-babu/mason-nvim-dap.nvim",
+		"williamboman/mason.nvim",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	config = function()
+		local mason_dap = require("mason-nvim-dap")
+		local dap = require("dap")
+		local ui = require("dapui")
+		local dap_virtual_text = require("nvim-dap-virtual-text")
+
+		dap_virtual_text.setup({})
+		mason_dap.setup({
+			ensure_installed = { "codelldb" },
+			automatic_installation = true,
+			handlers = {},
+		})
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/usr/bin/lldb", -- adjust as needed, must be absolute path
+			name = "lldb",
+		}
+
+		dap.defaults.fallback.external_terminal = {
+			command = "/opt/homebrew/bin/ghostty",
+		}
+		dap.defaults.fallback.terminal_win_cmd = "50vsplit new"
+
+		dap.configurations = {
+			c = {
+				{
+					name = "Launch file",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtEntry = false,
+					MIMode = "lldb",
+				},
+				{
+					name = "Attach to lldbserver :1234",
+					type = "codelldb",
+					request = "launch",
+					MIMode = "lldb",
+					miDebuggerServerAddress = "localhost:1234",
+					miDebuggerPath = "/usr/bin/lldb",
+					cwd = "${workspaceFolder}",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+				},
+			},
+		}
+		ui.setup()
+		vim.fn.sign_define("DapBreakpoint", { text = "B" })
+
+		dap.listeners.before.attach.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			ui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			ui.close()
+		end
+	end,
+	keys = {
+		-- Debugger
+		{
+			"<leader>d",
+			group = "Debugger",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>dt",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Toggle Breakpoint",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>dc",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Continue",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>di",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Step Into",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>do",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Step Over",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>du",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Step Out",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>dr",
+			function()
+				require("dap").repl.open()
+			end,
+			desc = "Open REPL",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>dl",
+			function()
+				require("dap").run_last()
+			end,
+			desc = "Run Last",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>dq",
+			function()
+				require("dap").terminate()
+				require("dapui").close()
+				require("nvim-dap-virtual-text").toggle()
+			end,
+			desc = "Terminate",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>db",
+			function()
+				require("dap").list_breakpoints()
+			end,
+			desc = "List Breakpoints",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>de",
+			function()
+				require("dap").set_exception_breakpoints({ "all" })
+			end,
+			desc = "Set Exception Breakpoints",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>d?",
+			function()
+				require("dapui").eval(nil, { enter = true })
+			end,
+			desc = "Evaluate Expression",
+			nowait = true,
+			remap = false,
+		},
+	},
+}
