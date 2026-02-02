381099cd6f4a8b7ea299149508f1d4953b11a026
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2026-02-02 19:58:08 +0800

    blink: update config

--- lua/plugins/blink-nvim.lua
+++ lua/plugins/blink-nvim.lua
@@ -59,15 +59,7 @@ return {
 			preset = "luasnip",
 		},
 		sources = {
-			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-			providers = {
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					-- make lazydev completions top priority (see `:h blink.cmp`)
-					score_offset = 100,
-				},
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 		fuzzy = {
 			implementation = "prefer_rust_with_warning",
