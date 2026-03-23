b777f164774a9f1c2aa7de4ad5b35aed660797e1
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2026-03-23 16:21:45 +0800

    feat: update inline diagnostics

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -14,23 +14,20 @@ vim.opt.number = true
 vim.opt.showmode = false
 
 -- text highlighting when yanking texts
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- config for LaTeX
 vim.g.tex_flavor = "latex"
 vim.cmd("filetype plugin on")
 
--- indentation flavor
-vim.g.python_recommend_style = 4
-
 -- inline diagnostics
 vim.diagnostic.config({
 	virtual_text = true,
-	virtual_line = true
+	virtual_line = true,
 })
